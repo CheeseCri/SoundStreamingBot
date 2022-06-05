7660ddbf86aeacbb364adf8d9dbe6f9922e1a5cb
KeonTeck Yun <yunghn3@gmail.com>
2022-06-05 21:09:57 +0900

    현재 떠있는 윈도우 text, process 출력하도록 작성

---
+++ ProcessManager.py
@@ -0,0 +1,33 @@
+"""
+ProcessManager.py
+* Window Application의 목록 탐색
+* 특정 Window Application을 선택하면, Application의 Processid 반환
+"""
+
+import logging
+import sys
+import win32api
+import win32gui
+import win32process
+
+
+def getActiveWindow():
+    activeWindowName = None
+
+    window = win32gui.GetForegroundWindow()
+    activeWindowList = win32gui.GetWindow()
+    print(activeWindowList)
+
+def winEnumHandelr(hwnd, ctx):
+    if win32gui.IsWindowVisible(hwnd):
+        windowName = win32gui.GetWindowText(hwnd)
+        processId = win32process.GetWindowThreadProcessId(hwnd)
+
+        Is
+        if windowName:
+            print(processId, windowName)
+
+
+
+if __name__ == "__main__":
+    win32gui.EnumWindows(winEnumHandelr, None)
